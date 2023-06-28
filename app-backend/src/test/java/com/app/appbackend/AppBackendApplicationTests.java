01f58f277d5a78d318a080de1b16f303ec445c39
Cui.Jiaqing <291527070@qq.com>
2023-06-28 17:19:50 +0800

    后端初始化

---
+++ app-backend/src/test/java/com/app/appbackend/AppBackendApplicationTests.java
@@ -0,0 +1,12 @@
+package com.app.appbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
